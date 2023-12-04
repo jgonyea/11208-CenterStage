5517f19b7b58e39a44fd8ded31bb789a3e6c4e40
jtiffse256 <jtiffse256@gmail.com>
2023-12-04 10:42:57 -0500

    Finish init function

--- Effector.java
+++ Effector.java
@@ -7,9 +7,23 @@ package org.firstinspires.ftc.teamcode.teamcode11208;
 import com.qualcomm.robotcore.hardware.Servo;
 
 public class Effector {
-    Servo armRotator;
+    Servo armRotatorLeft;
+    Servo armRotatorRight;
     Servo handRotator;
     Servo leftActuator;
     Servo rightActuator;
 
+    public void init(Servo armRotatorLeft, Servo armRotatorRight, Servo handRotator, Servo leftActuator, Servo rightActuator) {
+        this.armRotatorLeft = armRotatorLeft;
+        this.armRotatorRight = armRotatorRight;
+        this.handRotator = handRotator;
+        this.leftActuator = leftActuator;
+        this.rightActuator = rightActuator;
+
+        armRotatorLeft.setDirection(Servo.Direction.FORWARD);
+        armRotatorRight.setDirection(Servo.Direction.REVERSE);
+        handRotator.setDirection(Servo.Direction.REVERSE);
+        leftActuator.setDirection(Servo.Direction.FORWARD);
+        rightActuator.setDirection(Servo.Direction.REVERSE);
+    }
 }
