c41c8cecd75bd4a4fb091da0cf7c956bdf0798af
Jeremy Gonyea <jeremy.gonyea@gmail.com>
2023-12-08 13:33:29 -0500

    Servo stage logic added

--- Effector.java
+++ Effector.java
@@ -5,6 +5,7 @@ package org.firstinspires.ftc.teamcode.teamcode11208;
 
 import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 public class Effector {
     private Servo armRotatorLeft;
@@ -14,39 +15,40 @@ public class Effector {
     private Servo pincerRight;
     private Servo wristRotator;
     private enum EffectorState {
-        NORMAL_POSE,
-        MOVING_TO_INTAKE,
-        INTAKE_POSE,
-        MOVING_TO_NORMAL,
-        MOVING_BACKWARD_TURN_RESTRICTED,
-        MOVING_BACKWARD_AND_TURNING,
-        PLACING_POSE,
-        MOVING_FORWARD_AND_TURNING,
-        MOVING_FORWARD_TURN_RESTRICTED
+        DRIVING,
+        STAGED_INTAKE,
+        INTAKE,
+        STAGED_LIFT,
+        SCORING,
     }
 
     // Todo: Fix these fake values
-    private final static double ARM_NORMAL_POSITION = 0.2;
-    private final static double WRIST_NORMAL_POSITION = 0;
-    private final static double HAND_NORMAL_POSITION = 0.3;
-    private final static long NORMAL_TO_INTAKE_TIME = 500_000_000;
+    private final static double ARM_DRIVING_POSITION = 0;
     private final static double ARM_INTAKE_POSITION = 0.1;
-    private final static double WRIST_INTAKE_POSITION = -0.1;
-    private final static long INTAKE_TO_NORMAL_TIME = 500_000_000;
-    private final static long NORMAL_TO_TURNING_TIME = 1_000_000_000;
-    private final static double ARM_POSITION_TO_BEGIN_TURNING = 0.5;
-    private final static long TURNING_TO_PLACING_TIME = 1_000_000_000;
-    private final static double HAND_PLACING_POSITION = 0.7;
-    private final static double ARM_PLACING_POSITION = 0.8;
-    private final static double WRIST_PLACING_POSITION = 0.2;
+    private final static double ARM_SCORING_POSITION = 1;
+    private final static double ARM_STAGED_INTAKE_POSITION = 0.2;
+    private final static double ARM_STAGED_LIFT_POSITION = 0.5;
+
+    private final static double HAND_DRIVING_POSITION = 0.5;
+    private final static double HAND_INTAKE_POSITION = 0;
+    private final static double HAND_SCORING_POSITION = 1;
+
     private final static double PINCER_CLOSED_POSITION = 0;
-    private final static double PINCER_GRIPPING_POSITION = 0.2;
-    private final static long PLACING_TO_TURNING_TIME = 1_000_000_000;
-    private final static long TURNING_TO_NORMAL_TIME = 1_000_000_000;
+    private final static double PINCER_GRIPPING_POSITION = 0.1;
+
+    private final static double WRIST_INTAKE_POSITION = 0;
+    private final static double WRIST_SCORING_POSITION = 1;
+
+    // Timings
+    private ElapsedTime timer = new ElapsedTime();
+    private final static double STAGED_INTAKE_TIME = 3.0;
 
     private EffectorState currentState;
-    private long lastStateChangeTime;
+    private boolean is_a_pressed = false;
+    private boolean is_y_pressed = false;
 
+
+    // Configure effector Servos.
     public void init(Servo armRotatorLeft, Servo armRotatorRight, Servo wristRotator, Servo handActuator, Servo pincerLeft, Servo pincerRight) {
         this.armRotatorLeft = armRotatorLeft;
         this.armRotatorRight = armRotatorRight;
@@ -62,100 +64,114 @@ public class Effector {
         pincerLeft.setDirection(Servo.Direction.FORWARD);
         pincerRight.setDirection(Servo.Direction.REVERSE);
 
-        // Todo: move servos to normal positions
-
-        currentState = EffectorState.NORMAL_POSE;
+        currentState = EffectorState.DRIVING;
     }
 
-    public void moveEffector(Gamepad gamepad) {
+    public void moveEffector(Gamepad gp) {
         switch (currentState) {
-            case NORMAL_POSE:
-                if (gamepad.b) {
-                    armRotatorLeft.setPosition(ARM_INTAKE_POSITION);
-                    armRotatorRight.setPosition(ARM_INTAKE_POSITION);
-                    wristRotator.setPosition(WRIST_INTAKE_POSITION);
-                    currentState = EffectorState.MOVING_TO_INTAKE;
-                    lastStateChangeTime = System.nanoTime();
+            case DRIVING:
+                armRotatorLeft.setPosition(ARM_DRIVING_POSITION);
+                armRotatorRight.setPosition(ARM_DRIVING_POSITION);
+                handActuator.setPosition(HAND_DRIVING_POSITION);
+                wristRotator.setPosition(WRIST_INTAKE_POSITION);
+
+                // Monitor for arm rotation buttons A & Y.
+                if (gp.a && !gp.y){
+                    currentState = EffectorState.STAGED_INTAKE;
+                    timer.reset();
+                    this.is_a_pressed = true;
+                    this.is_y_pressed = false;
                 }
-                if (gamepad.x) {
-                    armRotatorLeft.setPosition(ARM_POSITION_TO_BEGIN_TURNING);
-                    armRotatorRight.setPosition(ARM_POSITION_TO_BEGIN_TURNING);
-                    currentState = EffectorState.MOVING_BACKWARD_TURN_RESTRICTED;
-                    lastStateChangeTime = System.nanoTime();
+                if (gp.y && ! gp.a) {
+                    currentState = EffectorState.STAGED_LIFT;
+                    this.is_a_pressed = false;
+                    this.is_y_pressed = true;
+                }
+
+                break;
+
+            case STAGED_INTAKE:
+                armRotatorLeft.setPosition(ARM_STAGED_INTAKE_POSITION);
+                armRotatorRight.setPosition(ARM_STAGED_INTAKE_POSITION);
+                handActuator.setPosition(HAND_INTAKE_POSITION);
+
+                if (timer.seconds() > STAGED_INTAKE_TIME) {
+                    if (is_a_pressed){
+                        currentState = EffectorState.INTAKE;
+                    } else {
+                        currentState = EffectorState.DRIVING;
+                    }
+
                 }
                 break;
-            case MOVING_TO_INTAKE:
-                if (System.nanoTime() - lastStateChangeTime >= NORMAL_TO_INTAKE_TIME) {
-                    currentState = EffectorState.INTAKE_POSE;
-                    lastStateChangeTime = System.nanoTime();
+
+            case INTAKE:
+                if (!gp.a) {
+                    this.is_a_pressed = false;
+                    currentState = EffectorState.STAGED_INTAKE;
+                    break;
+                }
+                armRotatorLeft.setPosition(ARM_INTAKE_POSITION);
+                armRotatorRight.setPosition(ARM_INTAKE_POSITION);
+                handActuator.setPosition(HAND_INTAKE_POSITION);
+                wristRotator.setPosition(WRIST_INTAKE_POSITION);
+
+                movePincers(gp, pincerLeft, pincerRight);
+
+                break;
+
+            case STAGED_LIFT:
+                armRotatorRight.setPosition(ARM_STAGED_LIFT_POSITION);
+                wristRotator.setPosition(WRIST_INTAKE_POSITION);
+
+                // Wait for 2nd press to go down.
+                if (gp.a && !this.is_a_pressed) {
+                    this.is_a_pressed = true;
+                    currentState = EffectorState.DRIVING;
+                }
+                if (!gp.a) {
+                    this.is_a_pressed = false;
+                }
+
+                // Wait for 2nd press to go up.
+                if (gp.y && !this.is_y_pressed) {
+                    this.is_y_pressed = true;
+                    currentState = EffectorState.SCORING;
+                }
+                if (!gp.y) {
+                    this.is_y_pressed = false;
                 }
                 break;
-            case INTAKE_POSE:
-                if (gamepad.left_bumper) {
-                    pincerLeft.setPosition(PINCER_GRIPPING_POSITION);
-                }
-                if (gamepad.right_bumper) {
-                    pincerRight.setPosition(PINCER_GRIPPING_POSITION);
-                }
-                if (gamepad.a) {
-                    armRotatorLeft.setPosition(ARM_NORMAL_POSITION);
-                    armRotatorRight.setPosition(ARM_NORMAL_POSITION);
-                    wristRotator.setPosition(WRIST_NORMAL_POSITION);
-                    currentState = EffectorState.MOVING_TO_NORMAL;
-                    lastStateChangeTime = System.nanoTime();
-                }
-                break;
-            case MOVING_TO_NORMAL:
-                if (System.nanoTime() - lastStateChangeTime >= INTAKE_TO_NORMAL_TIME) {
-                    currentState = EffectorState.NORMAL_POSE;
-                    lastStateChangeTime = System.nanoTime();
-                }
-            case MOVING_BACKWARD_TURN_RESTRICTED:
-                if (System.nanoTime() - lastStateChangeTime >= NORMAL_TO_TURNING_TIME) {
-                    armRotatorLeft.setPosition(ARM_PLACING_POSITION);
-                    armRotatorRight.setPosition(ARM_PLACING_POSITION);
-                    handActuator.setPosition(HAND_PLACING_POSITION);
-                    wristRotator.setPosition(WRIST_PLACING_POSITION);
-                    currentState = EffectorState.MOVING_BACKWARD_AND_TURNING;
-                    lastStateChangeTime = System.nanoTime();
+
+            case SCORING:
+                armRotatorRight.setPosition(ARM_SCORING_POSITION);
+                armRotatorLeft.setPosition(ARM_SCORING_POSITION);
+                handActuator.setPosition(HAND_SCORING_POSITION);
+                wristRotator.setPosition(WRIST_SCORING_POSITION);
+
+                movePincers(gp, pincerLeft, pincerRight);
+
+                if (gp.a) {
+                    currentState = EffectorState.STAGED_LIFT;
+                    is_a_pressed = true;
                 }
                 break;
-            case MOVING_BACKWARD_AND_TURNING:
-                if (System.nanoTime() - lastStateChangeTime >= TURNING_TO_PLACING_TIME) {
-                    currentState = EffectorState.PLACING_POSE;
-                    lastStateChangeTime = System.nanoTime();
-                }
-                break;
-            case PLACING_POSE:
-                if (gamepad.left_bumper) {
-                    pincerLeft.setPosition(PINCER_CLOSED_POSITION);
-                }
-                if (gamepad.right_bumper) {
-                    pincerRight.setPosition(PINCER_CLOSED_POSITION);
-                }
-                if (gamepad.a) {
-                    armRotatorLeft.setPosition(ARM_POSITION_TO_BEGIN_TURNING);
-                    armRotatorRight.setPosition(ARM_POSITION_TO_BEGIN_TURNING);
-                    handActuator.setPosition(HAND_NORMAL_POSITION);
-                    wristRotator.setPosition(WRIST_NORMAL_POSITION);
-                    currentState = EffectorState.MOVING_FORWARD_AND_TURNING;
-                    lastStateChangeTime = System.nanoTime();
-                }
-                break;
-            case MOVING_FORWARD_AND_TURNING:
-                if (System.nanoTime() - lastStateChangeTime >= PLACING_TO_TURNING_TIME) {
-                    armRotatorLeft.setPosition(ARM_NORMAL_POSITION);
-                    armRotatorRight.setPosition(ARM_NORMAL_POSITION);
-                    currentState = EffectorState.MOVING_FORWARD_TURN_RESTRICTED;
-                    lastStateChangeTime = System.nanoTime();
-                }
-                break;
-            case MOVING_FORWARD_TURN_RESTRICTED:
-                if (System.nanoTime() - lastStateChangeTime >= TURNING_TO_NORMAL_TIME) {
-                    currentState = EffectorState.NORMAL_POSE;
-                    lastStateChangeTime = System.nanoTime();
-                }
-                break;
+
+            default:
+                throw new IllegalStateException("Unexpected value: " + currentState);
+        }
+    }
+
+    private void movePincers(Gamepad gp, Servo pincerLeft, Servo pincerRight) {
+        if (gp.left_bumper) {
+            pincerLeft.setPosition(PINCER_CLOSED_POSITION);
+        } else {
+            pincerLeft.setPosition(PINCER_GRIPPING_POSITION);
+        }
+        if (gp.right_bumper) {
+            pincerRight.setPosition(PINCER_CLOSED_POSITION);
+        } else {
+            pincerRight.setPosition(PINCER_GRIPPING_POSITION);
         }
     }
 }
