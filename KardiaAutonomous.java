81ca4dde25c8d4df2448438b46056d9114c2f630
jtiffse256 <jtiffse256@gmail.com>
2023-12-11 11:45:14 -0500

    Add a simple trajectory to KardiaAutonomous

--- KardiaAutonomous.java
+++ KardiaAutonomous.java
@@ -4,33 +4,40 @@
 
 package org.firstinspires.ftc.teamcode;
 
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
+import com.acmerobotics.roadrunner.trajectory.Trajectory;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
 
 @Autonomous(name="KardiaAutonomous")
 public class KardiaAutonomous extends LinearOpMode {
 
     ElapsedTime gameTimer;
+    SampleMecanumDrive mecanumDrive;
+    Trajectory someTrajectory;
+    Pose2d lastPose;
     @Override
     public void runOpMode(){
         // Initialize and configure here.
         gameTimer = new ElapsedTime();
+        mecanumDrive = new SampleMecanumDrive(hardwareMap);
+        lastPose = new Pose2d(0, 0, 0);
+        mecanumDrive.setPoseEstimate(lastPose);
+        someTrajectory = mecanumDrive.trajectoryBuilder(lastPose)
+                .splineTo(new Vector2d(0, 10), Math.toRadians(45))
+                .splineTo(new Vector2d(40, 10),0)
+                .build();
 
         // Pause and wait for driver to press Start.
         waitForStart();
         gameTimer.reset();
 
-        // Autonomous routine here.
-        while (opModeIsActive()) {
-            if (gameTimer.seconds() > 30.0){
-                requestOpModeStop();  // Todo: Is this correct?
-            }
-
-            // Todo: Move robot to end-goal destination.
-        }
-
-        telemetry.addData("Label", "Data");
+        // Todo: Move robot to end-goal destination.
+        mecanumDrive.followTrajectory(someTrajectory);
 
     }
 }
