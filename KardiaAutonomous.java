3881800f0f6fc4157e2bbc2883aeac4fa00d130c
Jeremy Gonyea <jeremy.gonyea@gmail.com>
2024-01-24 18:55:57 -0500

    WIP Autonomous

--- KardiaAutonomous.java
+++ KardiaAutonomous.java
@@ -4,15 +4,21 @@
 
 package org.firstinspires.ftc.teamcode;
 
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.trajectory.Trajectory;
+import com.acmerobotics.roadrunner.trajectory.TrajectoryBuilder;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
+import org.firstinspires.ftc.teamcode.drive.CenterStageDrive;
+
 @Autonomous(name="KardiaAutonomous")
 public class KardiaAutonomous extends LinearOpMode {
 
-    ElapsedTime gameTimer;
+    ElapsedTime autonomousModeTimer = new ElapsedTime();
+    ElapsedTime stepTimer = new ElapsedTime();;
 
     Servo armRotatorLeft;
     Servo armRotatorRight;
@@ -22,12 +28,17 @@ public class KardiaAutonomous extends LinearOpMode {
     Servo wristRotator;
     Effector effector = new Effector();
 
-    private boolean first_event = true;
-    private boolean second_event = true;
+    // todo: add camera and vision processors.
+
+    // Increment as each autonoumous step proceeds.
+    private int step = 0;
+
+
+
     @Override
     public void runOpMode(){
         // Initialize and configure here.
-        gameTimer = new ElapsedTime();
+        CenterStageDrive robot = new CenterStageDrive(hardwareMap);
 
         // Effector hardware mapping.
         armRotatorLeft = hardwareMap.get(Servo.class, "armL");
@@ -44,27 +55,68 @@ public class KardiaAutonomous extends LinearOpMode {
 
         // Pause and wait for driver to press Start.
         waitForStart();
-        gameTimer.reset();
+        autonomousModeTimer.reset();
+
+        robot.setPoseEstimate(new Pose2d(15, -63, 90));
+
+        Trajectory begin = robot.trajectoryBuilder(robot.getPoseEstimate())
+                .splineToLinearHeading(new Pose2d(1, 2, 3), 0)
+                .build();
+
+        Trajectory nextTraj = robot.trajectoryBuilder(begin.end())
+                .splineToLinearHeading(new Pose2d(1, 2, 3), 0)
+                .build();
 
         while (opModeIsActive()) {
-            telemetry.addData("gameTimer", gameTimer.seconds());
-            telemetry.addData("floor(gameTimer)", Math.floor(gameTimer.seconds()));
+            telemetry.addData("Game Time: ", autonomousModeTimer.seconds());
 
-            if (Math.floor(gameTimer.seconds()) == 3 && first_event) {
-                first_event = false;
-                effector.setCurrentState(
-                        Effector.EffectorState.STAGED_INTAKE, false
-                );
-            }
-            if (Math.floor(gameTimer.seconds()) == 6 && second_event) {
-                second_event = false;
-                effector.setCurrentState(
-                        Effector.EffectorState.STAGED_INTAKE, true
-                );
+            // todo: fill in missing autonomous programming.
+
+            // Grab pixels from starting position.
+            if (step == 0) {
+                effector.setDesiredState(Effector.EffectorState.STAGED_INTAKE);
+                // todo: set pincer to closed
+
+                sleep(Effector.STAGED_INTAKE_TIME);
+                effector.setDesiredState(Effector.EffectorState.INTAKE);
+                // todo: set pincers to open
+
+                sleep(Effector.STAGED_INTAKE_TIME * 2);
+
+                effector.setDesiredState(Effector.EffectorState.DRIVING);
+
+                step++;
             }
 
-            effector.updateWithoutGamepad();
-            effector.moveEffector();
+            // Locate team prop and maneuver to proper location.
+            if (step == 1 && effector.getCurrentState() == Effector.EffectorState.DRIVING) {
+
+                step++;
+            }
+
+
+            if (step == 2) {
+
+                step++;
+            }
+
+
+
+
+            //Trajectory traj = robot.trajectoryBuilder(new Pose2d())
+            //        .splineTo(new Vector2d(30, 30), 0)
+            //        .build();
+            //
+            //robot.followTrajectory(traj);
+            // Place left (purple)
+            // Locate April tag for global positioning reorientation?
+            // Drive to score board
+            // Score right (yellow)
+            // Drive to parking spot
+
+            // Run modes of robot.
+            effector.run();
+
             telemetry.update();
         }
     }
