8ca77a0928de0e787cef489fb22286119605d6ec
jtiffse256 <jtiffse256@gmail.com>
2024-02-07 11:46:58 -0500

    Revise ServoTester

--- ServoTester.java
+++ ServoTester.java
@@ -6,7 +6,7 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.Servo;
 
 @Disabled
-@TeleOp(name="servo test thing")
+@TeleOp(name="Pincer Tester")
 public class ServoTester extends OpMode {
     Servo leftPincer;
     Servo rightPincer;
@@ -16,22 +16,24 @@ public class ServoTester extends OpMode {
     public void init() {
         leftPincer = hardwareMap.get(Servo.class, "pincerL");
         rightPincer = hardwareMap.get(Servo.class, "pincerR");
+        leftPincer.setPosition(0.4944);
+        rightPincer.setPosition(0.4344);
+
         currentPincer = leftPincer;
-        leftPincer.setPosition(0.4422);
-        rightPincer.setPosition(0.4939);
     }
 
     @Override
     public void loop() {
+        if (gamepad1.left_bumper)
+            currentPincer = leftPincer;
+        if (gamepad1.right_bumper)
+            currentPincer = rightPincer;
+
         currentPincer.setPosition(
                 currentPincer.getPosition() + (
                         gamepad1.right_trigger - gamepad1.left_trigger
                         ) / 1000
         );
         telemetry.addData("Current Pincer Position", currentPincer.getPosition());
-        if (gamepad1.left_bumper)
-            currentPincer = leftPincer;
-        if (gamepad1.right_bumper)
-            currentPincer = rightPincer;
     }
 }
