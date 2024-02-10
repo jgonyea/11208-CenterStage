f6e3d2ac5b063a290d23c747838cd1da77ed6f34
Jeremy Gonyea <jeremy.gonyea@gmail.com>
2024-02-09 21:38:58 -0500

    WIP KardiaAutoRedFar

--- KardiaAutoBlueFar.java
+++ KardiaAutoBlueFar.java
@@ -7,7 +7,6 @@ package org.firstinspires.ftc.teamcode;
 import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.acmerobotics.roadrunner.trajectory.Trajectory;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DistanceSensor;
