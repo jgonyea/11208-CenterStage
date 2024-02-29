e0d2d26107539010ecef0b49c90c443889bd58ed
jtiffse256 <jtiffse256@gmail.com>
2024-02-29 17:28:34 -0500

    Disable tester opmodes

--- FrontPincerTester.java
+++ FrontPincerTester.java
@@ -1,9 +1,11 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.Servo;
 
+@Disabled
 @TeleOp(name="Front Pincer Tester")
 public class FrontPincerTester extends OpMode {
     Servo leftServo;
--- SwitchTester.java
+++ SwitchTester.java
@@ -1,9 +1,11 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DigitalChannel;
 
+@Disabled
 @TeleOp(name="Config Switch Tester")
 public class SwitchTester extends OpMode {
     DigitalChannel leftSwitch;
