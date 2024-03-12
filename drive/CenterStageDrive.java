339dc2df1c306d0cef4450356b3583002a159930
jtiffse256 <jtiffse256@gmail.com>
2024-03-12 12:02:25 -0400

    Tune in auto drivetrain numbers
    If autonomous is suddenly broken, revert this commit!

--- drive/CenterStageDrive.java
+++ drive/CenterStageDrive.java
@@ -55,7 +55,7 @@ import java.util.List;
  */
 @Config
 public class CenterStageDrive extends MecanumDrive {
-    public static PIDCoefficients TRANSLATIONAL_PID = new PIDCoefficients(4.7, 0, 0.36);
+    public static PIDCoefficients TRANSLATIONAL_PID = new PIDCoefficients(6, 0, 0.2);
     public static PIDCoefficients HEADING_PID = new PIDCoefficients(6, 0, 0.2);
 
     public static double LATERAL_MULTIPLIER = 1;
